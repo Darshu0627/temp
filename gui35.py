d89eeb45b55f1ae87a3ec4409908ddb9f6be1452
Darshu0627 <darshanabhikonde2709@gmail.com>
2022-06-04 12:07:58 +0530

    changes

--- gui35.py
+++ gui35.py
@@ -6,13 +6,9 @@ def set_red_color(val):
       red="0"+red
     color="#"+red+"0000"
     root.configure(bg=color)
-    
-# root = Tk()
+root = Tk()
 root.geometry('200x200')
 sc=Scale(root,from_=0,to=255,orient=HORIZONTAL,command=set_red_color)
-
 sc.pack()
 root.configure(bg="#000")
-
-
 root.mainloop()
